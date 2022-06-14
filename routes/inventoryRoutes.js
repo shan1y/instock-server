381efdff1121f392b46b11b4c771a804c63d27cb
Michael Nguyen <n93michael@gmail.com>
2022-06-13 23:42:41 -0400

    minor variable changes. added in get request for all inventory data

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -5,10 +5,16 @@ const { v4: uuid } = require("uuid");
 
 router.use(express.json());
 
-const inventoryData = "./data/inventories.json";
+const inventoryDataPath = "./data/inventories.json";
+
+//FUNCTION TO READ FILE
+const readFile = () => {
+  return JSON.parse(fs.readFileSync(inventoryDataPath));
+};
 
 router.route("/").get((req, res) => {
-  res.send("test from inventory");
+  let inventoryData = readFile();
+  return res.status(200).send(inventoryData);
 });
 
 module.exports = router;
