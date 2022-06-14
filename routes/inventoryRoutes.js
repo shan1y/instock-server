94c999e5845e6a731524374ee0b9719acbc8a47e
Michael Nguyen <n93michael@gmail.com>
2022-06-14 07:37:29 -0400

    created route for get inventory item by id

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -12,9 +12,25 @@ const readFile = () => {
   return JSON.parse(fs.readFileSync(inventoryDataPath));
 };
 
+// GETTING ALL INVENTORY ITEMS DATA
 router.route("/").get((req, res) => {
-  let inventoryData = readFile();
-  return res.status(200).send(inventoryData);
+  let inventoryItems = readFile();
+  return res.status(200).send(inventoryItems);
 });
+
+router
+  .route("/:inventoryId")
+  // GETTING INVENTORY BY THEIR IDS
+  .get((req, res) => {
+    const inventoryItemId = req.params.inventoryId;
+    const inventoryItems = readFile().find(
+      (inventoryItem) => inventoryItem.id === inventoryItemId
+    );
+
+    // CONDITIONAL FOR URL VALIDATION
+    !inventoryItems
+      ? res.status(404).send("Item not found")
+      : res.status(200).json(inventoryItems);
+  });
 
 module.exports = router;
