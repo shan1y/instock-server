4dfd7812a349f853ab4a66441a332cd6b7f230de
yakiv-k <yakiv.y.k@gmail.com>
2022-06-17 15:54:02 -0400

    unknown changes

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -63,14 +63,6 @@ router
     !inventoryItems
       ? res.status(404).send("Item not found")
       : res.status(200).json(inventoryItems);
-  })
-  .delete((req, res) => {
-    const inventoryData = readFile();
-    const newInventoryList = inventoryData.filter((inventory) => {
-      return inventory.id !== req.params.inventoryId;
-    });
-    fs.writeFileSync(inventoryDataPath, JSON.stringify(newInventoryList));
-    res.status(200).json(newInventoryList);
   });
 
 router.route("/:inventoryId/edit").put((req, res) => {
